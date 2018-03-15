c6fbbf626f7ae79fe1edecbb4053b2ffecf90522
Jane Kim <Janeekim12@gmail.com>
2018-03-15 17:15:39 -0600

    finally got card numbers to validate

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -7,7 +7,7 @@ class CreditCheck
   end
 
   def convert_array
-    @number.reverse.split(//)
+    number.reverse.split(//)
   end
 
   def array_of_integers
@@ -24,6 +24,38 @@ class CreditCheck
        val
      end
     end
+  end
+
+  def sum_greater_than_9
+    double.map do |number|
+      if number > 9
+         num_digits = number.to_s.chars.map do |num|
+           num.to_i
+         end.reduce(:+)
+       else
+         number
+       end
+    end
+  end
+
+  def sumed_digits
+    sum = 0
+    sum_greater_than_9.each do |num|
+      sum += num
+    end
+    sum
+    # sum_greater_than_9.reduce(0) do |sum, num|
+    #   sum + num
+    # end
+  end
+
+  def validate
+    if sumed_digits % 10 == 0
+      return true
+    else
+      return false
+    end
+
   end
 
 end
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -18,23 +18,45 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_convert_to_array
-    skip
     cc = CreditCheck.new("4929735477250543")
 
-    assert_equal ["4", "9", "2", "9", "7", "3", "5", "4", "7", "7", "2", "5", "0", "5", "4", "3"], cc.convert_array
+    assert_equal  ["3", "4", "5", "0", "5", "2", "7", "7", "4", "5", "3", "7", "9", "2", "9", "4"], cc.convert_array
   end
 
   def test_convert_array_into_integers
-    skip
     cc = CreditCheck.new("4929735477250543")
 
-    assert_equal [4,9,2,9,7,3,5,4,7,7,2,5,0,5,4,3], cc.array_of_integers
+    assert_equal [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4], cc.array_of_integers
   end
 
   def test_double_every_other_number
     cc = CreditCheck.new("4929735477250543")
     # binding.pry
     assert_equal [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8], cc.double
+  end
+
+  def test_it_sums_numbers_greater_than_9
+    cc = CreditCheck.new("4929735477250543")
+
+    assert_equal [3, 8, 5, 0, 5, 4, 7, 5, 4, 1, 3, 5, 9, 4, 9, 8], cc.sum_greater_than_9
+  end
+
+  def test_sum_all_of_the_digits
+    cc = CreditCheck.new("4929735477250543")
+
+    assert_equal 80, cc.sumed_digits
+  end
+
+  def test_it_validates
+    cc = CreditCheck.new("4929735477250543")
+
+    assert cc.validate
+  end
+  def test_balh
+    cc_1
+    cc_2
+    assert cc_1.validate
+    refute cc_2.validate
   end
 
 end
