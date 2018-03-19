f5a4bedd699cda0cc28c06a7f92a4faa27b88d96
Jane Kim <Janeekim12@gmail.com>
2018-03-18 21:14:57 -0600

    refactor

--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -5,52 +5,40 @@ require 'pry'
 
 class CreditCheckTest < Minitest::Test
 
-  def test_it_exists
-    cc = CreditCheck.new("4929735477250543")
+  def setup
+    @cc = CreditCheck.new("4929735477250543")
+  end
 
-    assert_instance_of CreditCheck, cc
+  def test_it_exists
+    assert_instance_of CreditCheck, @cc
   end
 
   def test_it_has_a_number
-    cc = CreditCheck.new("4929735477250543")
-
-    assert_equal "4929735477250543", cc.number
+    assert_equal "4929735477250543", @cc.number
   end
 
   def test_convert_to_array
-    cc = CreditCheck.new("4929735477250543")
-
-    assert_equal  ["3", "4", "5", "0", "5", "2", "7", "7", "4", "5", "3", "7", "9", "2", "9", "4"], cc.convert_array
+    assert_equal  ["3", "4", "5", "0", "5", "2", "7", "7", "4", "5", "3", "7", "9", "2", "9", "4"], @cc.convert_array
   end
 
   def test_convert_array_into_integers
-    cc = CreditCheck.new("4929735477250543")
-
-    assert_equal [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4], cc.array_of_integers
+    assert_equal [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4], @cc.array_of_integers
   end
 
   def test_double_every_other_number
-    cc = CreditCheck.new("4929735477250543")
-
-    assert_equal [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8], cc.double
+    assert_equal [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8], @cc.double
   end
 
   def test_it_sums_numbers_greater_than_9
-    cc = CreditCheck.new("4929735477250543")
-
-    assert_equal [3, 8, 5, 0, 5, 4, 7, 5, 4, 1, 3, 5, 9, 4, 9, 8], cc.sum_greater_than_9
+    assert_equal [3, 8, 5, 0, 5, 4, 7, 5, 4, 1, 3, 5, 9, 4, 9, 8], @cc.sum_greater_than_9
   end
 
   def test_sum_all_of_the_digits
-    cc = CreditCheck.new("4929735477250543")
-
-    assert_equal 80, cc.sumed_digits
+    assert_equal 80, @cc.sumed_digits
   end
 
   def test_it_validates
-    cc = CreditCheck.new("4929735477250543")
-
-    assert cc.validate
+    assert @cc.validate
   end
 
   def test_amex_cards
